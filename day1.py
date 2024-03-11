033231f831e6bf3d8c3e979df7847db0af698503
saiteja2218 <162982769+saiteja2218@users.noreply.github.com>
2024-03-11 16:14:49 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,30 @@
+#single line commenting can be done using #at the begin of the line
+#print("hello world ")
+'''
+multiple line commenting can be done using ''' code '''.the lines b/w trhe quotes will be connected
+''
+'
+'''
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+'''
+print("hello world")
+
+#import is a keyword which is used to communication b/w the phytron files
+import this
+print(this)
+import antigravity
+print(antigravity)
+
+#BARGAINED CHARACTERS
+'''
+controls and data structures ->ABC languge
+strings and its format -> from C
+regular expressions -> pearl
+expection handing -> modules
+magic method from -> fortron
+
+'''
